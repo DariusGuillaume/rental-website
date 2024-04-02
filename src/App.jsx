da3894156d678594fac7847225018171a71a9b53
DariusGuillaume <dariusguillaume2@gmail.com>
2024-04-02 05:04:58 -0400

    navbar top menu added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
+import Navbar from './components/navbar/Navbar'
 import './layout.scss'
 
 function App() {
   return (
     <div className="layout"> 
-      <header className="header">
-        <h1>React App</h1>
-      </header>
-      <main className="content">
-        <p>Welcome to the React App</p>
-      </main>
-      <footer className="footer">
-        <p>© 2021</p>
-      </footer>
+    <Navbar/>
     
     </div>
   )
