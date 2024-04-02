c6c7e1ed4c05561e7cd0408d9bea47b89f81fd30
DariusGuillaume <dariusguillaume2@gmail.com>
2024-04-02 04:29:36 -0400

    added responsive movement layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import './layout.scss'
+
 function App() {
   return (
-    <div> <a href = "/" > Home </a>
+    <div className="layout"> 
+      <header className="header">
+        <h1>React App</h1>
+      </header>
+      <main className="content">
+        <p>Welcome to the React App</p>
+      </main>
+      <footer className="footer">
+        <p>© 2021</p>
+      </footer>
     
     </div>
   )
