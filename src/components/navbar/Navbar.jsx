9bfa98d4d368fc761ff43d76a67ee880f56a6bd6
DariusGuillaume <dariusguillaume2@gmail.com>
2024-04-02 06:52:45 -0400

    added mobile responsive navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import "./navbar.scss"
+import {useState} from "react"
 
 function Navbar (){
-
+const [open, setOpen] = useState(false);
 return (
     <nav> 
     <div className="left"> 
@@ -18,10 +19,21 @@ return (
     <div className="right"> 
     <a href="/">Login</a>
     <a href="/" className="register">Register</a>
+    <div className="menu-icon">
+        <img src = "/menu.png" alt="" onClick={() => setOpen((prev)=> !prev)} />
+</div>
+
+<div className={`menu ${open ? 'active' : ''}`}>
+  <a href="/">Home</a>
+  <a href="#">About</a>
+  <a href="#">Contact</a>
+  <a href="#">Services</a>
+  <a href="#">Login</a>
+  <a href="#">Register</a>
+</div>
     </div>
-    
     </nav>
-)
+);
 
 }
 
